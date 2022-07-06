dc633c0204f1b6630143e70fbb6b7f38e521e924
eyinpei <108116826+eyinpei@users.noreply.github.com>
2022-07-06 15:04:40 +0800

    Update mineeeper.c

--- mineeeper.c
+++ mineeeper.c
@@ -3,73 +3,65 @@
 #include<math.h>
 #include <malloc.h> 
 
-
 int main(){
     int n,m;
+    char str[100][100];
+    
     while(~scanf("%d %d",&n,&m)){
-        char str[n][m];
         for(int i = 0;i <n;i++){
-            gets(str[i]);
+            for(int j = 0;j <m;j++){
+                scanf("%c",&str[i][j]);
+            }
             getchar();
         }
+
         int count;
         for(int i = 0;i <n;i++){
             for(int j = 0;j <m;j++){
                 count = 0;
-                if(str[i][j]=='*'){
-                    count =-1;
-                    // printf("str[%d][%d]=%d\n",i,j,count);
-                }
-                if(str[i][j]=='.'){
-                    if(j-1>=0){
-                        if(str[i][j]=='*'){
-                            count= count+1;
-                        }
-                        if(i-1>=0&&str[i][j]=='*'){
-                            count= count+1;
-                        }
-                        if(i+1<n&&str[i][j]=='*'){
-                            count= count+1;
-                        }
-                    }
-                    if(i-1>=0&&str[i][j]=='*'){
-                        count= count+1;
-                        }
-                        if(i+1<n&&str[i][j]=='*'){
-                            count= count+1;
-                    }
-                    if(j+1<m){
-                        if(str[i][j]=='*'){
-                            count= count+1;
-                        }
-                        if(i-1>=0&&str[i][j]=='*'){
-                            count= count+1;
-                        }
-                        if(i+1<n&&str[i][j]=='*'){
-                            count= count+1;
-                        }
-                    }
+
+                if(str[i][j] == '*'){
+                    printf("%c",str[i][j]);
                 }
                 
-                // str[i][j] = count;
-                printf("str[%d][%d]=%d\n",i,j,count);
+                if(str[i][j]=='.'){
+                    if(j-1>=0){
+                        if(str[i][j-i]=='*'){
+                            count= count+1;
+                        }
+                        if(i-1>=0&&str[i-1][j-1]=='*'){
+                            count= count+1;
+                        }
+                        if(i+1<n&&str[i+1][j-1]=='*'){
+                            count= count+1;
+                        }
+                    }
+                    if(i-1>=0&&str[i-1][j]=='*'){
+                        count= count+1;
+                        }
+                    if(i+1<n&&str[i+1][j]=='*'){
+                        count= count+1;
+                    }
+                    if(j+1<m){
+                        if(str[i][j+1]=='*'){
+                            count= count+1;
+                        }
+                        if(i-1>=0&&str[i-1][j+1]=='*'){
+                            count= count+1;
+                        }
+                        if(i+1<n&&str[i+1][j-1]=='*'){
+                            count= count+1;
+                        }
+                    }
+                    printf("%d",count);
+                }  
             }
+            printf("\n");
         }
-        
-    for(int i = 0;i <n;i++){
-        for(int j = 0;j <m;j++){
-            printf("%d",str[i][j]);
-        }
-        printf("\n");
-    }
-    
     }
 
     return 0;
 }
-
-
-
 
 
 
