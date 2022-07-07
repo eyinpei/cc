c09689f6bdb7bddaf57a11c14ba666526649f1ea
eyinpei <108116826+eyinpei@users.noreply.github.com>
2022-07-07 11:11:38 +0800

    Update mineeeper.c

--- mineeeper.c
+++ mineeeper.c
@@ -3,65 +3,91 @@
 #include<math.h>
 #include <malloc.h> 
 
+
 int main(){
     int n,m;
     char str[100][100];
     
     while(~scanf("%d %d",&n,&m)){
-        for(int i = 0;i <n;i++){
-            for(int j = 0;j <m;j++){
-                scanf("%c",&str[i][j]);
-            }
-            getchar();
+        if(n==0&&m==0){
+            break;
         }
 
-        int count;
-        for(int i = 0;i <n;i++){
-            for(int j = 0;j <m;j++){
-                count = 0;
-
-                if(str[i][j] == '*'){
-                    printf("%c",str[i][j]);
+        else{
+            for(int i = 0;i <n;i++){                        //输入扫雷地图
+                for(int j = 0;j <m;j++){
+                    scanf("%c",&str[i][j]);
                 }
-                
-                if(str[i][j]=='.'){
-                    if(j-1>=0){
-                        if(str[i][j-i]=='*'){
-                            count= count+1;
-                        }
-                        if(i-1>=0&&str[i-1][j-1]=='*'){
-                            count= count+1;
-                        }
-                        if(i+1<n&&str[i+1][j-1]=='*'){
-                            count= count+1;
-                        }
-                    }
-                    if(i-1>=0&&str[i-1][j]=='*'){
-                        count= count+1;
-                        }
-                    if(i+1<n&&str[i+1][j]=='*'){
-                        count= count+1;
-                    }
-                    if(j+1<m){
-                        if(str[i][j+1]=='*'){
-                            count= count+1;
-                        }
-                        if(i-1>=0&&str[i-1][j+1]=='*'){
-                            count= count+1;
-                        }
-                        if(i+1<n&&str[i+1][j-1]=='*'){
-                            count= count+1;
-                        }
-                    }
-                    printf("%d",count);
-                }  
+                getchar();
             }
-            printf("\n");
+
+            int count = 0,leftj,rightj;
+            for(int i = 0;i <n;i++){
+                for(int j = 0;j <m;j++){
+
+                    if(str[i][j] =='*'){
+                        printf("%c",str[i][j]);
+                    }
+
+                    
+                    if(str[i][j] =='.'){
+
+                        leftj = j-1;                         //左边三排
+                        if(leftj>=0){                         
+                            if(str[i][leftj]=='*'){
+                                count= count+1;
+                            }
+                            if(i-1>=0&&str[i-1][leftj]=='*'){
+                                count= count+1;
+                            }
+                            if(i+1<n&&str[i+1][leftj]=='*'){
+                                count= count+1;
+                            }
+                        }
+
+                        if(i-1>=0&&str[i-1][j]=='*'){       //正上
+                            count= count+1;
+                            }
+                        if(i+1<n&&str[i+1][j]=='*'){        //正下
+                            count= count+1;
+                        }
+
+                        rightj = j+1;
+                        if(rightj<m){                          //右边三排
+                            if(str[i][rightj]=='*'){
+                                count= count+1;
+                            }
+                            if(i-1>=0&&str[i-1][rightj]=='*'){
+                                count= count+1;
+                            }
+                            if(i+1<n&&str[i+1][rightj]=='*'){
+                                count= count+1;
+                            }
+                        }
+                        printf("%d",count);
+                        count = 0;
+                    }  
+
+                    
+                }
+                printf("\n");
+            }
+
         }
+        
+
+        
+        
     }
 
     return 0;
 }
+
+
+
+
+
+
 
 
 
